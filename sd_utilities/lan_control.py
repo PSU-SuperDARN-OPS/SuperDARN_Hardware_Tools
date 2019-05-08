1e8b3eb8099947b6c54828f1cca74b4d148eac30
bgklug <bgklug@alaska.edu>
2019-05-08 12:38:16 -0800

    Fix formatting and spelling issues

--- sd_utilities/lan_control.py
+++ sd_utilities/lan_control.py
@@ -1,32 +1,38 @@
-import telnetlib, time
-VNATIMEOUT = 5 # seconds
+import telnetlib
+import time
+
+VNATIMEOUT = 5  # seconds
 NE5230APORT = 5024
 VNAHOST = '192.168.11.3'
 
-def lan_init(host, port = 23):
-    tn = telnetlib.Telnet(host, port)
-    time.sleep(.5)
-    print("initial response from VNA: " + tn.read_until("MAGIC", timeout=5))
-    return tn 
 
-def lan_send(tn, command, verbose=False, wait=True):
+def lan_init(host, port=23):
+    telnet_connection = telnetlib.Telnet(host, port)
+    time.sleep(.5)
+    print("initial response from VNA: " + telnet_connection.read_until("MAGIC", timeout=5))
+    return telnet_connection
+
+
+def lan_send(telnet_connection, command, verbose=False, wait=True):
     if wait:
-        tn.write(command + ';*WAI\r\n')
+        telnet_connection.write(command + ';*WAI\r\n')
     else:   
-        tn.write(command + '\r\n')
+        telnet_connection.write(command + '\r\n')
 
     time.sleep(.05)
-    response = tn.read_until('>', VNATIMEOUT)
-    response = response[:-7] # strip trailing SCPI>\r\n
-    if(verbose):
-        print(str(command) + ', reponse: ' + str(response))
+    response = telnet_connection.read_until('>', VNATIMEOUT)
+    response = response[:-7]  # strip trailing SCPI>\r\n
+    if verbose:
+        print(str(command) + ', response: ' + str(response))
     return response
 
-def lan_close(tn):
-    response = tn.read_very_lazy() # check if there is anything left in the buffer..
+
+def lan_close(telnet_connection):
+    response = telnet_connection.read_very_lazy()  # check if there is anything left in the buffer..
     if response != '':
-        print('uh oh.. we found something in the recieve buffer: ' + str(response))
-    tn.close()
+        print('uh oh.. we found something in the receive buffer: ' + str(response))
+    telnet_connection.close()
+
 
 if __name__ == '__main__':
     tn = lan_init(VNAHOST, NE5230APORT)
