d5df5b24949d1373c4dd760f894f0cad6d3da8df
bgklug <bgklug@alaska.edu>
2019-05-08 12:51:38 -0800

    Fix formatting and spelling issues

--- cablelen_cal/cable_len.py
+++ cablelen_cal/cable_len.py
@@ -4,7 +4,13 @@
 from __future__ import division
 from sd_utilities.vna_control import *
 from numpy import mean, deg2rad, pi, floor
-import argparse, logging, os, sys, csv, datetime, pdb
+import argparse
+import logging
+import os
+import sys
+import csv
+import datetime
+import pdb
 
 FSTART = 5e6 
 FSTOP = 25e6
@@ -12,15 +18,16 @@ FSTOP = 25e6
 SWEEP_CENTER = 15e6
 SWEEP_SPAN = 20e6
 SWEEP_POINTS = 1201
-TIMEOUT = max(0.1,0.04/201.0 * SWEEP_POINTS)
+TIMEOUT = max(0.1, 0.04/201.0 * SWEEP_POINTS)
 VELOCITY_FACTOR = .87
 C = 3e8
-TOLERANCE = .02 # meters of cable length tolerance
-CABLE_LEN = 150 # cable length in meters
-APERTURE = 2.0 # smoothing aperture for vna time delay measurements
+TOLERANCE = .02  # meters of cable length tolerance
+CABLE_LEN = 150  # cable length in meters
+APERTURE = 2.0  # smoothing aperture for vna time delay measurements
 
 FEET_PER_METER = 3.28084
 INCHES_PER_METER = FEET_PER_METER * 12
+
 
 def printlog(s):
     print(s)
@@ -41,6 +48,7 @@ def get_cablelen(vna, vel, freqs):
 
     return clen_delay
 
+
 def calc_cablelen_ephase(vna, vel, freqs, fstart = FSTART, fstop = FSTOP):
     ephase = vna_readextendedphase(vna)
     fstep = freqs[1] - freqs[0]
@@ -54,6 +62,7 @@ def calc_cablelen_ephase(vna, vel, freqs, fstart = FSTART, fstop = FSTOP):
     clen_ephase = tdelay * C * vel / 2.0
     return clen_ephase
 
+
 def calc_cablelen_delay(vna, vel):
     tdelay = vna_readtimedelay(vna)
     inset = int(len(tdelay) * 1.1 * APERTURE / 100)
@@ -61,27 +70,28 @@ def calc_cablelen_delay(vna, vel):
     clen_delay = tavg * C * vel / 2.0
     return clen_delay
 
+
 if __name__ == '__main__':
-    # setup arguement parser and parse arguements
+    # setup argument parser and parse arguments
     parser = argparse.ArgumentParser()
 
     parser.add_argument("--cal", action="count", help="run through calibration on vna before taking measurements", default=0)
     parser.add_argument("--meas", action="count", help="only measure length of cable, then exit", default=0)
-    parser.add_argument("--vnaip", help="specify vna ip address", default=VNAHOST)
-    parser.add_argument("--ddir", help="specify a directory to save the data in", default='sandbox')
+    parser.add_argument("--vna_ip", help="specify vna ip address", default=VNAHOST)
+    parser.add_argument("--data_dir", help="specify a directory to save the data in", default='sandbox')
     parser.add_argument("--avg", type=int, help="specify count to average", default=1)
     parser.add_argument("--cables", type=int, help="specify number of cables to measure", default=1)
     parser.add_argument("--vel", type=float, help="specify a velocity factor for the cable", default=VELOCITY_FACTOR)
-    parser.add_argument("--tlen", type=float, help="specify a target cable length (in meters)", default=CABLE_LEN)
+    parser.add_argument("--target_len", type=float, help="specify a target cable length (in meters)", default=CABLE_LEN)
     parser.add_argument("--tol", type=float, help="specify cable length tolerance (in meters)", default=TOLERANCE)
     args = parser.parse_args()
 
-    # sanity check arguements 
+    # sanity check arguments
     if args.avg < 1:
         sys.exit("error: average count is less than 1")
 
     if not os.path.exists(args.ddir):
-        sys.exit("error: data directory does not exist: %s" % (args.ddir))
+        sys.exit("error: data directory does not exist: %s" % args.ddir)
 
     if args.cables < 1:
         sys.exit("error: cable count is less than 1")
@@ -89,15 +99,14 @@ if __name__ == '__main__':
     if args.vel > 1 or args.vel < 0:
         sys.exit("error: cable velocity factor violates known physics")
 
-
     if args.tlen == 0:
         sys.exit("error: target cable length is zero meters")
 
     # setup logging and csv file writing
-    logging.basicConfig(format='%(asctime)s %(message)s', filename= args.ddir + '/cable_len' + str(datetime.date.today()) + '.log', filemode='a')
+    logging.basicConfig(format='%(asctime)s %(message)s', filename=args.ddir + '/cable_len' + str(datetime.date.today()) + '.log', filemode='a')
 
     csvfile = open(args.dir + '/cable_lens' + str(datetime.date.today()) + '.csv', 'a')
-    csvwriter = csv.writer(csvfile, delimiter = ',')
+    csvwriter = csv.writer(csvfile, delimiter=',')
     
     csvwriter.writerow(["cable length log", str(datetime.date.today())])
     csvwriter.writerow(["cable #", "cable length (m)"])
@@ -129,7 +138,6 @@ if __name__ == '__main__':
     
     cable_lens = {}
 
-        
     for p in range(args.cables):
         p = int(raw_input('connect and enter a cable number and then press enter to continue... '))
 
@@ -156,7 +164,6 @@ if __name__ == '__main__':
 
             printlog('cable %d error is %.4f, cut %.4f meters (%.4f feet and %.4f inches)' % (p, error, error / 2.0, floor(feeterror), incherror))
             raw_input('press enter to continue\n')
-
 
     lan_close(vna)
 
